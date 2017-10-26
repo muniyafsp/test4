c167640c70daf9cca1c03b18582a1a18470b2466
muniyafsp <munisamyfsp@outlook.com>
2017-10-26 15:02:40 +0530

    add config file into develop branch

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_name="develop_test4";
+$db_uname="develop_test";
+$db_pwd="develop_test123";
+
+echo "DB Name: ".$db_name.'------'."DB UserName: ".$db_uname.'------'.'DB Password: '.$db_pwd;
+?>
